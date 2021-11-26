7446f342ef4d90893828cb9c02f81f2ec9557514
Junki Nakagawa <junki.dev@gmail.com>
2021-11-26 14:25:03 +0900

    display dynamice news and case-study in homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Blog;
+use App\Post;
 use App\Category;
 use App\Housing;
+use App\Result;
 use Illuminate\Support\Facades\DB;
 class HomeController extends Controller
 {
@@ -15,6 +17,8 @@ class HomeController extends Controller
         $blogs = $blogs->concat($categories);
         $blogs=  Blog::join('categories','blogs.category','=','categories.id')->take(10)->get(["blogs.*","categories.id as category_id","categories.name as category_name"]);
         $houses = Housing::orderBy('updated_at','DESC')->paginate(8);
-        return view('index', compact('blogs', 'houses'));
+        $posts = Post::orderBy('updated_at','DESC')->paginate(4);
+        $results = Result::orderBy('updated_at','DESC')->paginate(4);
+        return view('index', compact('blogs', 'houses', 'posts', 'results'));
     }
 }
