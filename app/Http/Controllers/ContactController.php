d31bb557db5479c951ab7341cb78f55edc8ea6ba
Junki Nakagawa <junki.dev@gmail.com>
2021-12-04 01:19:21 +0900

    change error message text

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -7,39 +7,80 @@ use Mail;
 
 class ContactController extends Controller
 {
-    public function contact(){
-        return view('contact');
-    }
-    public function document_request(){
-        return view('document-request');
-    }
+  public function contact() {
+    return view('contact');
+  }
+  public function document_request() {
+    return view('document-request');
+  }
 
-    public function document_requestPost(Request $request){
-        $this->validate($request, [
-                        'name' => 'required',
-                        'email' => 'required|email',
-                        'content' => 'required',
-                        'companyName' => 'required',
-                        'phoneNumber' => 'required',
-                        'purpose' => 'required',
-                        'content' => 'required',
-                        'agree' => 'required'
+  public function document_requestPost(Request $request){
+    $this->validate($request, [
+        'name' => 'required',
+        'email' => 'required|email',
+        'content' => 'required',
+        'companyName' => 'required',
+        'phoneNumber' => 'required',
+        'purpose' => 'required',
+        'content' => 'required',
+        'agree' => 'required'
+    ]);
 
-                ]);
-       $name = $request->name;
-        $email = $request->email;
-        $title = "title";
-        $content = $request->content;
+    $name = $request->name;
+    $email = $request->email;
+    $title = "title";
+    $content = $request->content;
+
+    \Mail::send('visitor_email', ['name' => $name, 'email' => $email, 'title' => $title, 'content' => $content], function ($message) {
+
+        $message->to('testuserxxx923@gmail.com')->subject('Subject of the message!');
+
+    });
+
+    return view('contact-done');
+      
+
+      /* Mail::send('visitor_email', [
+      'title' => "no title",
+      'name' => $request->get('name'),
+      'email' => $request->get('email'),
+      'content' => $request->get('content') ],
+      function ($message) {
+              $message->from('info@e-shinwa.net');
+              $message->to('redapple961129@hotmail.com', 'Your Name')
+                      ->subject('Your Website Contact Form');
+      }); */
+
+    return view('contact-done');#back()->with('success', 'Thanks for contacting me, I will get back to you soon!');
+
+  }
+
+  public function contactPost(Request $request){
+    $this->validate($request, [
+        'name' => 'required',
+        'email' => 'required|email',
+        'content' => 'required',
+        'companyName' => 'required',
+        'phoneNumber' => 'required',
+        'purpose' => 'required',
+        'content' => 'required',
+        'agree' => 'required'
+
+    ]);
+    $name = $request->name;
+    $email = $request->email;
+    $title = "title";
+    $content = $request->content;
+
+
+    \Mail::send('visitor_email', ['name' => $name, 'email' => $email, 'title' => $title, 'content' => $content], function ($message) {
+
+        $message->to('testuserxxx923@gmail.com')->subject('Subject of the message!');
+    });
     
-    
-        \Mail::send('visitor_email', ['name' => $name, 'email' => $email, 'title' => $title, 'content' => $content], function ($message) {
+    return view('contact-done');
 
-            $message->to('testuserxxx923@gmail.com')->subject('Subject of the message!');
-        });
-        return view('contact-done');
-        
-
-        /* Mail::send('visitor_email', [
+    /*  Mail::send('visitor_email', [
         'title' => "no title",
         'name' => $request->get('name'),
         'email' => $request->get('email'),
@@ -48,48 +89,9 @@ class ContactController extends Controller
                 $message->from('info@e-shinwa.net');
                 $message->to('redapple961129@hotmail.com', 'Your Name')
                         ->subject('Your Website Contact Form');
-        }); */
+    });*/
 
-        return view('contact-done');#back()->with('success', 'Thanks for contacting me, I will get back to you soon!');
+    return view('contact-done');#back()->with('success', 'Thanks for contacting me, I will get back to you soon!');
 
-    }
-
-    public function contactPost(Request $request){
-        $this->validate($request, [
-            'name' => 'required',
-            'email' => 'required|email',
-            'content' => 'required',
-            'companyName' => 'required',
-            'phoneNumber' => 'required',
-            'purpose' => 'required',
-            'content' => 'required',
-            'agree' => 'required'
-
-        ]);
-       $name = $request->name;
-        $email = $request->email;
-        $title = "title";
-        $content = $request->content;
-    
-    
-        \Mail::send('visitor_email', ['name' => $name, 'email' => $email, 'title' => $title, 'content' => $content], function ($message) {
-
-            $message->to('testuserxxx923@gmail.com')->subject('Subject of the message!');
-        });
-        return view('contact-done');
-
-       /*  Mail::send('visitor_email', [
-            'title' => "no title",
-            'name' => $request->get('name'),
-            'email' => $request->get('email'),
-            'content' => $request->get('content') ],
-            function ($message) {
-                    $message->from('info@e-shinwa.net');
-                    $message->to('redapple961129@hotmail.com', 'Your Name')
-                            ->subject('Your Website Contact Form');
-        });*/
-
-        return view('contact-done');#back()->with('success', 'Thanks for contacting me, I will get back to you soon!');
-
-    }
+  }
 }
--- resources/views/document-request.blade.php
+++ resources/views/document-request.blade.php
@@ -19,22 +19,22 @@
       <div class="input-group">
         <label for="">会社名*</label>
         <input type="text" name = "companyName" id = "companyName" placeholder="入力してください">
-        <span class="text-danger">{{ $errors->first('companyName') }}</span>
+        <span class="text-danger">{{ $errors->first('companyName')!= ""? "*必須項目に入力してください。":"" }}</span>
       </div>
       <div class="input-group">
         <label for="">氏名*</label>
         <input type="text" name = "name" id = "name" placeholder="入力してください">
-        <span class="text-danger">{{ $errors->first('name') }}</span>
+        <span class="text-danger">{{ $errors->first('name')!= ""? "*必須項目に入力してください。":"" }}</span>
       </div>
       <div class="input-group">
         <label for="">メールアドレス*</label>
         <input type="text" type = "email" name = "email" id = "email" placeholder="入力してください">
-        <span class="text-danger">{{ $errors->first('email') }}</span>
+        <span class="text-danger">{{ $errors->first('email')!= ""? "*必須項目に入力してください。":"" }}</span>
       </div>
       <div class="input-group">
         <label for="">電話番号*</label>
         <input type="text" id = "phoneNumber" name = "phoneNumber" placeholder="入力してください">
-        <span class="text-danger">{{ $errors->first('phoneNumber') }}</span>
+        <span class="text-danger">{{ $errors->first('phoneNumber')!= ""? "*必須項目に入力してください。":"" }}</span>
       </div>
       <div class="input-group">
         <label for="">資料請求の目的を選択してください*</label>
@@ -56,17 +56,17 @@
       <div class="input-group textarea-group">
         <label for="">ご相談、要望などあればご記入ください</label>
         <textarea  id="content" name = "content" placeholder="入力してください"></textarea>
-        <span class="text-danger">{{ $errors->first('content') }}</span>
+        <span class="text-danger">{{ $errors->first('content')!= ""? "*必須項目に入力してください。":"" }}</span>
       </div>
       <div class="privacy">
         <div class=""><a href="">プライバシーポリシー</a><br class="hidden sm:block">に同意の上、送信ください。</div>
         <div class="radio-group">
           <label class="radio-container">プライバシーポリシーに同意する
             <input type="radio" name="agree" value = "agree">
-            <span class="text-danger">{{ $errors->first('agree') != ""? "You have to agree":"" }}</span>
             <span class="checkmark"></span>
           </label>
         </div>
+        <span class="text-danger">{{ $errors->first('agree')!= ""? "*必須項目に入力してください。":"" }}</span>
       </div>
       <div class="btn-container">
         <button type="submit" id = "submit" class="btn hover-spacing-btn">送信する<img src="{{ URL::asset('images/ico_triangle.png') }}" alt=""></button>
